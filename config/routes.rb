1910d96731c237c29316bf08284299e50be6a391
Tatsuya Ogi <t.oginogin@gmail.com>
2013-05-14 09:03:43 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root to: 'static_pages#home'
   
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/about',   to: 'static_pages#about'
   match '/home',    to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
